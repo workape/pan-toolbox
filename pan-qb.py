19d942405082e6183f7686121ec2e0a017744d21
workape <workape@gmail.com>
2017-10-20 08:58:35 -0700

    Updated to add in CSV file backup capabilities.

--- pan-qb.py
+++ pan-qb.py
@@ -16,9 +16,10 @@ requests.packages.urllib3.disable_warnings()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Quick Backup of onboard firewall configuration')
-    parser.add_argument('-fw', type=str, help='IP Address of Firewall', required=True)
-    parser.add_argument('-k', '--api_key', type=str, help='API Key with access to Firewall', required=True)
-    parser.add_argument('-out', type=str, help='Output file where the configuration should be written', required=True)
+    parser.add_argument('-fw', type=str, help='IP Address of Firewall')
+    parser.add_argument('-k', '--api_key', type=str, help='API Key with access to Firewall')
+    parser.add_argument('-out', type=str, help='Output file where the configuration should be written')
+    parser.add_argument('-fwlist', type=str, help='CSV file of firewalls to backup')
     args = parser.parse_args()
     return parser, args
 
@@ -31,9 +32,21 @@ def pull_backup(fw, api_key, outfile):
     config_out.close()
 
 
+def pull_firewalllist(fwlist):
+    with open(fwlist, 'r') as fwdata:
+        for line in fwdata:
+            (fw, apikey) = line.split(',')
+            pull_backup(fw, apikey, '%s.cfg' % fw.replace('.', '_'))
+
+
 def control():
     backup_parser, backup_args = parse_args()
-    pull_backup(backup_args.fw, backup_args.api_key, backup_args.out)
+    if backup_args.fw and backup_args.api_key and backup_args.out:
+        pull_backup(backup_args.fw, backup_args.api_key, backup_args.out)
+    elif backup_args.fwlist:
+        pull_firewalllist(backup_args.fwlist)
+    else:
+        backup_parser.print_help()
 
 
 if __name__ == '__main__':
