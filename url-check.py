79eefce353c31519cb5865720aa5fe37271421ae
workape <workape@gmail.com>
2018-02-20 07:13:45 -0700

    Added debugging and additional error checking, Thanks for the data James!

--- url-check.py
+++ url-check.py
@@ -3,6 +3,7 @@
 import requests
 import lxml.etree
 import argparse
+import sys
 requests.packages.urllib3.disable_warnings()
 
 '''
@@ -18,27 +19,38 @@ def parse_args():
     parser.add_argument('-k', '--api_key', type=str, help='API Key with access to Firewall', required=True)
     parser.add_argument('-ui', '--urls_in', type=str, help='Input file containing a list of URLS for querying', required=True)
     parser.add_argument('-uo', '--urls_out', type=str, help='Output file where the URL and category are written in CSV', required=True)
+    parser.add_argument('-d', '--debug', type=bool, default=False, help='Enable Debugging for additional Outputs')
     args = parser.parse_args()
     return parser, args
 
 
-def url_query(fw, api_key, urls_in, urls_out):
+def url_query(fw, api_key, urls_in, urls_out, debug):
     resp_out = open(urls_out, 'a')
     with open(urls_in, 'r') as urls_file:
         for url in urls_file:
             test_headers = {'type': 'op', 'key': api_key, 'cmd': '<test><url-info-cloud>' + url + '</url-info-cloud></test>'}
             url_req = requests.get('https://%s/api' % fw, params=test_headers, verify=False)
-            resp_root = lxml.etree.fromstring(url_req.content)
-            for x in resp_root.xpath('//result')[0].text.split():
-                if ',' in x:
-                    y = x.split(',')
-                    print y[0], y[-1]
-                    resp_out.write('%s,%s\n' % (y[0], y[-1]))
+            if debug:
+                print '***Response from API***'
+                print url_req.content
+                print '***End API Response\n\n'
+            if url_req.status_code is requests.codes.ok:
+                resp_root = lxml.etree.fromstring(url_req.content)
+                if resp_root.xpath('//result') is not None:
+                    for x in resp_root.xpath('//result')[0].text.split():
+                        if debug:
+                            print '***Data in //result xpath: %s' % x
+                        if ',' in x:
+                            y = x.split(',')
+                            print y[0], y[-1]
+                            resp_out.write('%s,%s\n' % (y[0], y[-1]))
+            else:
+                sys.exit('API Connection to %s returned %s' % (fw, url_req.status_code))
 
 
 def control():
     url_parser, url_args = parse_args()
-    url_query(url_args.fw, url_args.api_key, url_args.urls_in, url_args.urls_out)
+    url_query(url_args.fw, url_args.api_key, url_args.urls_in, url_args.urls_out, url_args.debug)
 
 
 if __name__ == '__main__':
